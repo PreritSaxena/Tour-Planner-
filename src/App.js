2909ac0140f46f3e5b18eeddec412f7926d38f87
PreritSaxena <preritsaxena2103@gmail.com>
2023-11-01 16:14:38 +0530

    tour planner

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import data from './data'
+import Tours from "./components/Tours";
+
+const App = () => {
+
+  const [tours,setTour] = useState(data)
+
+   function removeTour(id){
+      const newTour = tours.filter(tour=> tour.id !==id);
+      setTour(newTour);
+   }
+
+   if(tours.length === 0){
+    return(
+      <div className="refresh">
+         <h2>No Tours Left</h2>
+         <button className="btn-white" onClick={() => setTour(data)}>
+         refresh
+         </button>
+      </div>
+    )
+   }
+
+    return (
+      <div className="App">
+     
+        <Tours tours={tours} removeTour={removeTour}></Tours>
+      </div> 
+    )
+};
+
+export default App;
